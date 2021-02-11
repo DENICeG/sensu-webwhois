8e1a24d337f2cb38f9a423f4c1096f4f41ce3467
danielb <git@masterunit.net>
2021-02-11 16:29:52 +0100

    v2.1.1

--- cmd/sensu-webwhois/main.go
+++ cmd/sensu-webwhois/main.go
@@ -30,6 +30,8 @@ func main() {
 func run() {
 	var err error
 	log.SetOutput(os.Stderr)
+	log.SetPrefix("UTC ")
+	log.SetFlags(log.Ltime | log.Lmsgprefix | log.LUTC)
 
 	if httpResp != nil {
 		httpResp.Body.Close() // nolint:errcheck
